31557c755c3cc62c5348ad939cff28b50f5ff24b
iamayushraj <yoayushraj99@gmail.com>
2021-02-20 23:38:37 +0530

    added Doubly_linkedLists.py

--- DS/Linked Lists/doubly_linkedLists.py
+++ DS/Linked Lists/doubly_linkedLists.py
@@ -1,52 +1,49 @@
-# Node class
 class Node:
-
-    # Function to initialize the node object
     def __init__(self, data):
-        self.data = data  # Assign data
-        self.next = None  # Initialize next as null
+        self.data = data
+        self.next = None
+        self.prev = None
 
 
-class LinkedLists:
+class DoublyLinkedList:
+
     def __init__(self, value):
-        self.head = {'value': value, 'next': None}
+        self.head = {'prev': None, 'value': value, 'next': None}
         self.tail = self.head
-        self.length = 1
+        self.length = 0
 
     def __str__(self):
         return str(self.__dict__)
 
     def append(self, value):
-        new_node = {'value': value, 'next': None}
+        new_node = {'prev': self.tail, 'value': value, 'next': None}
         self.tail['next'] = new_node
         self.tail = new_node
         self.length += 1
         return self.tail
 
     def prepend(self, value):
-        new_node = {'value': value, 'next': self.head}
+        new_node = {'prev': None, 'value': value, 'next': self.head}
         self.head = new_node
         self.length += 1
-        return self.head, self.length
-    
+        return self.head
+
     def traverse_to_node(self, index):
         current_node = self.head
         for i in range(index - 1):
             current_node = current_node['next']
         return current_node
-    
+
     def insert(self, index, value):
         if index == 0:
             return self.prepend(value)
         elif index == self.length - 1:
             return self.append(value)
         else:
-            new_node = {'value': value, 'next': None}
-            current_node = self.traverse_to_node(index)
-            new_node['next'] = current_node['next']
-            current_node['next'] = new_node
+            new_node = {'prev': self.traverse_to_node(index), 'value': value, 'next': self.traverse_to_node(index)['next']}
+            self.traverse_to_node(index)['next'] = new_node
             self.length += 1
-            return self.head, self.length
+            return self.head
 
     def remove(self, index, value):
         if index == 0:
@@ -54,15 +51,11 @@ class LinkedLists:
             return self.head
         else:
             current_node = self.traverse_to_node(index)
-            new_node = current_node['next']['next']
-            current_node['next'] = new_node
+            current_node['next'] = current_node['next']['next']
+            self.length -= 1
             return self.head
 
 
-myLinkedLists = LinkedLists(10)
-# print(myLinkedLists)
-print(myLinkedLists.append(10))
-print(myLinkedLists.append(8))
-print(myLinkedLists.prepend(5))
-print(myLinkedLists.insert(2, 4))
-print(myLinkedLists.remove(2, 4))
+linked_list = DoublyLinkedList(2)
+print(linked_list.append(3))
+print(linked_list)
